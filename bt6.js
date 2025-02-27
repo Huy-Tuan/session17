21dc05262ed09ef240750dbf28de8981173e03cb
Huy-Tuan <sieuka1990@gmail.com>
2025-02-27 11:20:54 +0700

    Add files via upload

--- bt6.js
+++ bt6.js
@@ -1,2 +1,17 @@
-let number = 5;
-let 
+// Chuyển đổi thành số: 
+let number1 = parseInt(5);
+let number2 = parseFloat(-1);
+let number3 = Number("Hello");
+
+// Chuyển đổi thành chuỗi:
+let str1 = String(55);
+let str2 = String(-10);
+let str3 = String(0);
+
+console.log(number1 + ":" + " " + typeof(number1));
+console.log(number2 + ":" + " " + typeof(number2));
+console.log(number3 + ":" + " " + typeof(number3));
+
+console.log(str1 + ":" + " " + typeof(str1));
+console.log(str2 + ":" + " " + typeof(str2));
+console.log(str3 + ":" + " " + typeof(str3));
